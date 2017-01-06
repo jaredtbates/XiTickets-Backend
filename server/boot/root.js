2254aefa3a43b48882d6d341e9b4a2a6d0704583
Jared Bates <computerwizjared@hotmail.com>
2017-01-06 12:51:23 -0600

    Move loopback status to /api and send static message to /

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,7 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', (req, res, next) => res.send('XiTickets Backend API (<a href="https://github.com/XiTickets/Backend">https://github.com/XiTickets/Backend</a>)'));
+  router.get('/api', server.loopback.status());
   server.use(router);
 };
